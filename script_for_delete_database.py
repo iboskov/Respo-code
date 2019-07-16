b9e2bf0b74d4a68249b27b5a0db545aceb85a916
CheBato <matevz.ogrinc@hotmail.com>
2019-07-16 09:02:59 +0200

    added script for deleting

---
+++ script_for_delete_database.py
@@ -0,0 +1,25 @@
+from home.models import *
+
+print("deleting workplaces")
+workplace.objects.all().delete()
+print("deleting employee")
+employee.objects.all().delete()
+print("deleting competence_type")
+competence_type.objects.all().delete()
+print("deleting competence")
+competence.objects.all().delete()
+print("deleting competece_relevance")
+competence_relevance.objects.all().delete()
+print("deleting trainings")
+education.objects.all().delete()
+print("deleting employee_competence")
+employee_competence.objects.all().delete()
+print("deleting participation")
+participation.objects.all().delete()
+print("deleting user")
+user.objects.all().delete()
+print("delete notifications")
+notifications.objects.all().delete()
+
+print("***FINISHED DATABASE CLEAN***")
+
